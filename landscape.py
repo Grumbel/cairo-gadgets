405e5795bb3fa7181c99b64929a065613f295db1
Ingo Ruhnke <grumbel@gmail.com>
2015-04-05 20:23:02 +0200

    Loosend the dependencies on Gtk for landscape.py

--- landscape.py
+++ landscape.py
@@ -46,105 +46,101 @@ def gen_segments(a, b, depth, midfunc):
 
     return loop(a, b, depth) + [b]
 
-# Create a GTK+ widget on which we will draw using Cairo
+
+def draw_landscape(cr):
+    width = cr.get_group_target().get_width()
+    height = cr.get_group_target().get_height()
+
+    # Fill the background with gray
+    cr.set_source_rgb(0.0, 0.0, 0.0)
+    cr.rectangle(0, 0, width, height)
+    cr.fill()
+
+    for i in range(0, 500):
+        gray = random.random()
+        cr.set_source_rgb(gray, gray, gray)
+        cr.arc(width * random.random() + 0.5,
+               height * random.random() + 0.5,
+               0.1, 0.0, 2 * math.pi)
+        cr.stroke()
+
+    c = [random.random(),
+         random.random(),
+         random.random()]
+
+    xof = (random.random() - 0.5) * 2.0
+    yof = (random.random() - 0.5) * 2.0
+
+    for i in range(0, random.randint(1, 2)):
+        x = random.randint(0, width)
+        y = random.randint(0, height // 2)
+        radius = random.randint(5, 72)
+
+        draw_moon(cr, x, y, radius, xof * radius, yof * radius,
+                  [c[0] * (1.0 - random.random() / 10.0),
+                   c[1] * (1.0 - random.random() / 10.0),
+                   c[2] * (1.0 - random.random() / 10.0)])
+
+    y = height / 2.0 * 1.5
+    n = 64
+
+    for i in range(0, n):
+        cr.set_source_rgb(((i + 1) / float(n) * c[0]) ** 2.2,
+                          ((i + 1) / float(n) * c[1]) ** 2.2,
+                          ((i + 1) / float(n) * c[2]) ** 2.2)
+        draw_mountain(cr, y + 2 ** (7.0 * (float(i) / (n - 1))), width, height)
 
 
-class Screen(Gtk.DrawingArea):
+def draw_moon(cr, x, y, radius, xof, yof, c):
+    cr.set_source_rgb(c[0], c[1], c[2])
+    cr.arc(x, y, radius * 1.1, 0.0, 2 * math.pi)
+    cr.fill()
 
-    def __init__(self):
-        super().__init__()
+    cr.set_source_rgb(c[0] * 2.0, c[1] * 2.0, c[2] * 2.0)
+    cr.arc(x, y, radius, 0.0, 2 * math.pi)
+    cr.fill()
 
-    def do_draw(self, cr):
-        self.draw(cr,
-                  int(self.get_allocated_width()),
-                  int(self.get_allocated_height()))
+    cr.save()
+    cr.arc(x, y, radius * 1.2, 0.0, 2 * math.pi)
+    cr.clip()
 
-    def draw(self, cr, width, height):
-        # Fill the background with gray
-        cr.set_source_rgb(0.0, 0.0, 0.0)
-        cr.rectangle(0, 0, width, height)
-        cr.fill()
-
-        for i in range(0, 500):
-            gray = random.random()
-            cr.set_source_rgb(gray, gray, gray)
-            cr.arc(width * random.random() + 0.5,
-                   height * random.random() + 0.5,
-                   0.1, 0.0, 2 * math.pi)
-            cr.stroke()
-
-        c = [random.random(),
-             random.random(),
-             random.random()]
-
-        xof = (random.random() - 0.5) * 2.0
-        yof = (random.random() - 0.5) * 2.0
-
-        for i in range(0, random.randint(1, 2)):
-            x = random.randint(0, width)
-            y = random.randint(0, height // 2)
-            radius = random.randint(5, 72)
-
-            self.draw_moon(cr, x, y, radius, xof * radius, yof * radius,
-                           [c[0] * (1.0 - random.random() / 10.0),
-                            c[1] * (1.0 - random.random() / 10.0),
-                            c[2] * (1.0 - random.random() / 10.0)])
-
-        y = height / 2.0 * 1.5
-        n = 64
-
-        for i in range(0, n):
-            cr.set_source_rgb(((i + 1) / float(n) * c[0]) ** 2.2,
-                              ((i + 1) / float(n) * c[1]) ** 2.2,
-                              ((i + 1) / float(n) * c[2]) ** 2.2)
-            self.draw_mountain(cr, y + 2 ** (7.0 * (float(i) / (n - 1))), width, height)
-
-    def draw_moon(self, cr, x, y, radius, xof, yof, c):
-        cr.set_source_rgb(c[0], c[1], c[2])
-        cr.arc(x, y, radius * 1.1, 0.0, 2 * math.pi)
-        cr.fill()
-
-        cr.set_source_rgb(c[0] * 2.0, c[1] * 2.0, c[2] * 2.0)
-        cr.arc(x, y, radius, 0.0, 2 * math.pi)
-        cr.fill()
-
-        cr.save()
-        cr.arc(x, y, radius * 1.2, 0.0, 2 * math.pi)
-        cr.clip()
-
-        cr.new_path()
-        cr.set_source_rgb(0, 0, 0)
-        cr.arc(x + xof,
-               y + yof,
-               radius * 1.5, 0.0, 2 * math.pi)
-        cr.fill()
-        # cr.reset_clip()
-        cr.restore()
-
-    def draw_mountain(self, cr, y, width, height):
-        points = gen_segments(y + (random.random() - 0.5) * 128.0,
-                              y + (random.random() - 0.5) * 128.0,
-                              8,
-                              lambda a, b, d: (a + b) / 2.0 + (random.random() - 0.5) * (height / 3.0) / 2 ** d)
-
-        cr.move_to(0, height)
-        for idx, p in enumerate(points):
-            cr.line_to(width / float(len(points) - 1) * idx, p)
-        cr.line_to(width, height)
-        cr.fill()
-
-# GTK mumbo-jumbo to show the widget in a window and quit when it's closed
+    cr.new_path()
+    cr.set_source_rgb(0, 0, 0)
+    cr.arc(x + xof,
+           y + yof,
+           radius * 1.5, 0.0, 2 * math.pi)
+    cr.fill()
+    # cr.reset_clip()
+    cr.restore()
 
 
-def run(Widget):
+def draw_mountain(cr, y, width, height):
+    width = cr.get_group_target().get_width()
+    height = cr.get_group_target().get_height()
+
+    points = gen_segments(y + (random.random() - 0.5) * 128.0,
+                          y + (random.random() - 0.5) * 128.0,
+                          8,
+                          lambda a, b, d: (a + b) / 2.0 + (random.random() - 0.5) * (height / 3.0) / 2 ** d)
+
+    cr.move_to(0, height)
+    for idx, p in enumerate(points):
+        cr.line_to(width / float(len(points) - 1) * idx, p)
+    cr.line_to(width, height)
+    cr.fill()
+
+
+def main():
     window = Gtk.Window()
     window.set_size_request(640, 480)
     window.connect("delete-event", Gtk.main_quit)
 
     vbox = Gtk.VBox()
 
-    widget = Widget()
+    widget = Gtk.DrawingArea()
     widget.show()
+
+    widget.connect("draw", lambda widget, cr: draw_landscape(cr))
 
     button = Gtk.Button("Regenerate")
     button.connect("clicked", lambda ev: widget.queue_draw())
@@ -161,7 +157,7 @@ def run(Widget):
 
 
 if __name__ == "__main__":
-    run(Screen)
+    main()
 
 
 # EOF #
