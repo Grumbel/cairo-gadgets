32880ade1b12623cd048c82c0fbcfbf25efb1d34
Ingo Ruhnke <grumbel@gmail.com>
2015-04-05 20:22:06 +0200

    Ported spiral.py to Python3/Gtk3

--- spiral.py
+++ spiral.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 # cairo-gadgets - A collection of gadgets for cairo
 # Copyright (C) 2015 Ingo Ruhnke <grumbel@gmail.com>
@@ -16,108 +16,91 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pygtk
-pygtk.require('2.0')
-import gtk
+
+from gi.repository import Gtk
 import random
 import math
 
-# TODO: reuse seed, allow changing of slope
 
-# Create a GTK+ widget on which we will draw using Cairo
+def draw_spirals(cr):
+    width = cr.get_group_target().get_width()
+    height = cr.get_group_target().get_height()
 
+    # Fill the background with gray
+    cr.set_source_rgb(0.0, 0.0, 0.0)
+    cr.rectangle(0, 0, width, height)
+    cr.fill()
 
-class Screen(gtk.DrawingArea):
-
-    # Draw in response to an expose-event
-    __gsignals__ = {"expose-event": "override"}
-
-    # Handle the expose-event by drawing
-    def do_expose_event(self, event):
-
-        # Create the cairo context
-        cr = self.window.cairo_create()
-
-        # Restrict Cairo to the exposed area; avoid extra work
-        cr.rectangle(event.area.x, event.area.y,
-                     event.area.width, event.area.height)
-        cr.clip()
-
-        self.draw(cr, *self.window.get_size())
-
-    def draw(self, cr, width, height):
-        # Fill the background with gray
-        cr.set_source_rgb(0.0, 0.0, 0.0)
-        cr.rectangle(0, 0, width, height)
-        cr.fill()
-
-        self.draw_spiral(cr,
-                         width / 2.0, height / 5.0 * 4.0,
-                         0,
-                         width / 2.0,
-                         random.random() * 0.1 + 0.1)
-
-    def draw_spiral(self, cr, x, y, angle, length, angle_delta):
-        if length > 20.0:
-            nangle = angle + angle_delta
-            seg = length / 30.0
-
-            dx = seg * math.sin(nangle)
-            dy = -seg * math.cos(nangle)
-
-            # cr.set_source_rgb(random.random(),
-            #                  random.random(),
-            #                  random.random())
-            # cr.set_line_width(2.0)
-            cr.set_source_rgb(1, 1, 1)
-            cr.move_to(x,
-                       y)
-            cr.line_to(x + dx,
-                       y + dy)
-            cr.stroke()
-
-            if random.randint(0, 15) == 0:
-                self.draw_spiral(cr,
-                                 x + dx,
-                                 y + dy,
-                                 nangle,
-                                 length - seg,
-                                 -angle_delta)
-
-            self.draw_spiral(cr,
-                             x + dx,
-                             y + dy,
-                             nangle,
-                             length - seg,
-                             angle_delta)
-
-# GTK mumbo-jumbo to show the widget in a window and quit when it's closed
+    draw_spiral(cr,
+                width / 2.0, height / 5.0 * 4.0,
+                0,
+                width / 2.0,
+                random.random() * 0.1 + 0.1)
 
 
-def run(Widget):
-    window = gtk.Window()
+def draw_spiral(cr, x, y, angle, length, angle_delta):
+    if length > 20.0:
+        nangle = angle + angle_delta
+        seg = length / 30.0
+
+        dx = seg * math.sin(nangle)
+        dy = -seg * math.cos(nangle)
+
+        # cr.set_source_rgb(random.random(),
+        #                  random.random(),
+        #                  random.random())
+        # cr.set_line_width(2.0)
+        cr.set_source_rgb(1, 1, 1)
+        cr.move_to(x,
+                   y)
+        cr.line_to(x + dx,
+                   y + dy)
+        cr.stroke()
+
+        if random.randint(0, 15) == 0:
+            draw_spiral(cr,
+                        x + dx,
+                        y + dy,
+                        nangle,
+                        length - seg,
+                        -angle_delta)
+
+        draw_spiral(cr,
+                    x + dx,
+                    y + dy,
+                    nangle,
+                    length - seg,
+                    angle_delta)
+
+
+def main():
+    window = Gtk.Window()
     window.set_size_request(640, 480)
-    window.connect("delete-event", gtk.main_quit)
+    window.connect("delete-event", Gtk.main_quit)
 
-    vbox = gtk.VBox()
+    vbox = Gtk.VBox()
 
-    widget = Widget()
+    widget = Gtk.DrawingArea()
     widget.show()
 
-    button = gtk.Button("Regenerate")
+    widget.connect("draw", lambda widget, cr: draw_spirals(cr))
+
+    button = Gtk.Button("Regenerate")
     button.connect("clicked", lambda ev: widget.queue_draw())
     button.show()
 
-    vbox.pack_start(widget, True, True)
-    vbox.pack_start(button, False, True)
+    vbox.pack_start(widget, True, True, 0)
+    vbox.pack_start(button, False, True, 0)
     vbox.show()
 
     window.add(vbox)
 
     window.present()
-    gtk.main()
+    Gtk.main()
+
 
 if __name__ == "__main__":
-    run(Screen)
+    main()
+
 
 # EOF #
