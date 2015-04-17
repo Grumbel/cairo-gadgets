d6403844549060df939fd685219091438f9c87b9
Ingo Ruhnke <grumbel@gmail.com>
2015-04-17 13:40:23 +0200

    Ported `shatter-early.py` to Applet

--- shatter-early.py
+++ shatter-early.py
@@ -17,8 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from gi.repository import Gtk
-import random
+from applet import Applet
 
 
 def midpoint(lst):
@@ -30,61 +29,45 @@ def midpoint(lst):
     return (x / len(lst), y / len(lst))
 
 
-def jitter(p, r):
-    return (p[0] + (random.random() * 2.0 - 1.0) * r,
-            p[1] + (random.random() * 2.0 - 1.0) * r)
+def draw(ctx):
+    cr = ctx.cr
+    width = ctx.width
+    height = ctx.height
+
+    # Fill the background with gray
+    cr.set_source_rgb(0.0, 0.0, 0.0)
+    cr.rectangle(0, 0, width, height)
+    cr.fill()
+
+    cr.set_source_rgba(1.0, 1.0, 1.0, 0.5)
+
+    draw_line(ctx, cr,
+              (10, height / 2.0),
+              (width - 10, height / 2.0),
+              10)
+
+    draw_line(ctx, cr,
+              (10, height / 2.0),
+              (width - 10, height / 2.0),
+              10)
+    cr.stroke()
 
 
-# Create a GTK+ widget on which we will draw using Cairo
-class Screen(Gtk.DrawingArea):
-
-    # Handle the expose-event by drawing
-
-    def do_draw(self, cr):
-        width = self.get_allocated_width()
-        height = self.get_allocated_height()
-
-        # Fill the background with gray
-        cr.set_source_rgb(0.0, 0.0, 0.0)
-        cr.rectangle(0, 0, width, height)
-        cr.fill()
-
-        cr.set_source_rgba(1.0, 1.0, 1.0, 0.5)
-
-        self.draw_line(cr,
-                       (10, height / 2.0),
-                       (width - 10, height / 2.0),
-                       10)
-
-        self.draw_line(cr,
-                       (10, height / 2.0),
-                       (width - 10, height / 2.0),
-                       10)
-        cr.stroke()
-
-    def draw_line(self, cr, a, b, r):
-        if r == 0:
-            cr.move_to(a[0], a[1])
-            cr.line_to(b[0], b[1])
-        else:
-            m = midpoint([a, b])
-            m = (m[0], m[1] + (random.random() * 2.0 - 1.0) * (2.0 ** r) * 0.1)
-            self.draw_line(cr, a, m, r - 1)
-            self.draw_line(cr, m, b, r - 1)
-
-
-def run(Widget):
-    window = Gtk.Window()
-    window.connect("delete-event", Gtk.main_quit)
-    widget = Widget()
-    widget.show()
-    window.add(widget)
-    window.present()
-    Gtk.main()
+def draw_line(ctx, cr, a, b, r):
+    if r == 0:
+        cr.move_to(a[0], a[1])
+        cr.line_to(b[0], b[1])
+    else:
+        m = midpoint([a, b])
+        m = (m[0], m[1] + (ctx.random.random() * 2.0 - 1.0) * (2.0 ** r) * 0.1)
+        draw_line(ctx, cr, a, m, r - 1)
+        draw_line(ctx, cr, m, b, r - 1)
 
 
 if __name__ == "__main__":
-    run(Screen)
+    applet = Applet()
+    applet.set_size(256, 256)
+    applet.run(draw)
 
 
 # EOF #
