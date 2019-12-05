ad7f650b6947ea0e665e9c25e84e3732410b8bf8
Ingo Ruhnke <grumbel@gmail.com>
2019-12-05 16:47:25 +0100

    Require a specific gtk version

--- gadgets/applet.py
+++ gadgets/applet.py
@@ -14,9 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 import random
-
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GObject
 from context import Context
 
